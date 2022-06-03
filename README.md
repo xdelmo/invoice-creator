4c7cee189ff4220c7f1e2e158a13e952e4f42e27
Emanuele <84925809+xdelmo@users.noreply.github.com>
2022-06-03 19:13:09 +0200

    v1.2 reset button added

---
+++ README.md
@@ -0,0 +1,87 @@
+# Scrimba - Invoice Creator solution
+
+This is a solution to the [Invoice Creator solo project on Scrimba](https://scrimba.com/learn/learnjavascript/). Scrimba helps you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Choose which tasks they want to receive
+- Get charged just once for same service
+- Always see updated services' list with single cost
+- Get automated total amount cost
+- Reset the whole demand clicking on "Send invoice" button
+- See hover states for interactive elements
+
+### Screenshot
+
+![screenshot](./screenshots/screenshot.png)
+
+### Links
+
+- Solution URL: [@GitHub](https://github.com/xdelmo/invoice-creator)
+- Live Site URL: [@GitHub](https://htmlpreview.github.io/?https://github.com/xdelmo/invoice-creator/blob/master/index.html)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid Layout
+- Vanilla Javascript
+
+### What I learned
+
+The newest thing I learnt is:
+
+- How to get and manipulate a substring
+
+```js
+// Create a function getCost() which returns service's cost
+function getCost(services) {
+  // The substring method will return the part of the string after the specified character ("$")
+  // However, we don't want to include the character in the returned string, so we increment its index by 1.
+  // Then use parseInt("", 10) to convert a string into an integer of the specified radix: in this case 10
+  return parseInt(services.substring(services.indexOf("$") + 1), 10);
+}
+```
+
+### Continued development
+
+I'd like to:
+
+- Use localStorage's browser to save tasks
+- Add the ability to remove tasks one by one
+
+### Useful resources
+
+- [StackOverlow](https://stackoverflow.com/questions/62762993/how-to-push-innerhtml-of-the-clicked-button-into-an-array-using-addevenlistener) - How to push innerHTML of the clicked button into an array
+- [W3Schools](https://www.w3schools.com/jsref/jsref_includes_array.asp) - How to use includes() method
+- [GoMakeThings](https://gomakethings.com/converting-strings-to-numbers-with-vanilla-javascript/) - Converting strings to numbers with vanilla JavaScript
+- [BoddyHadz](https://bobbyhadz.com/blog/javascript-get-substring-after-specific-character) - Get the Substring after a specific Character in JavaScript
+
+## Author
+
+- Website - [Emanuele Del Monte](https://www.emanueledelmonte.it)
+
+## Acknowledgments
+
+A big thank you to [Per Harald Borgen](https://github.com/perborgen) who is a pleasure to listen to and to learn new this in funny and interactive way 👏
--- scripts/index.js
+++ scripts/index.js
@@ -1,57 +1,60 @@
+// Create and assign myServices to an empty array
 let myServices = [];
+let totalCost = 0;
+
+// Store the button elements into variables
 const carBtn = document.getElementById("carBtn");
 const lawnBtn = document.getElementById("lawnBtn");
 const weedsBtn = document.getElementById("weedsBtn");
+const sendBtn = document.getElementById("sendBtn");
+
+// Store the servicesList element into variables
+const servicesList = document.getElementById("servicesList");
+// Store totalNotes, totalAmount into variables
 const totalNotes = document.getElementById("totalNotes");
 const totalAmount = document.getElementById("totalAmount");
-const servicesList = document.getElementById("servicesList");
 
-// Listen for clicks on inputBtn
-
+// Listen for clicks on button
 carBtn.addEventListener("click", function () {
+  // Charge just once for same service
   if (myServices.includes(this.innerHTML)) return;
+  // Add whole button's phrase to myServices array
   myServices.push(this.innerHTML);
-  console.log(myServices);
+  // Call render() function
   render(myServices);
 });
 
+// Listen for clicks on button
 lawnBtn.addEventListener("click", function () {
+  // Charge just once for same service
   if (myServices.includes(this.innerHTML)) return;
+  // Add whole button's phrase to myServices array
   myServices.push(this.innerHTML);
-  console.log(myServices);
+  // Call render() function
   render(myServices);
 });
 
+// Listen for clicks on button
 weedsBtn.addEventListener("click", function () {
+  // Charge just once for same service
   if (myServices.includes(this.innerHTML)) return;
+  // Add whole button's phrase to myServices array
   myServices.push(this.innerHTML);
-  console.log(myServices);
+  // Call render() function
   render(myServices);
 });
 
-function update(myServices) {
-  myServices.push(this.innerHTML);
-  console.log(myServices);
-  render(myServices);
-}
-
-// Push the value from the inputEl into the myServices array
-//myServices.push(carBtn.value);
-// Clear out the input field
-//carBtn.value = "";
-//console.log(myServices);
-// Save the myServices array to localStorage after transformed it into a string
-//localStorage.setItem("myServices", JSON.stringify(myServices));
-// Call render() with myServices as argument
-//render(myServices);
-
+// Create a function render() to display services selected
 function render(services) {
+  // Create a variable, listItems, to hold all the HTML for the list items
   let listItems = "";
-  let cost = 0;
-  let totalCost = 0;
+  // Assign totalCost to 0
+  totalCost = 0;
+  //  Add the items in the services array to listItems using a for loop
   for (let i = 0; i < services.length; i++) {
     cost = getCost(services[i]);
     totalCost += cost;
+    // Use "template string" with backtick to concatenate directly HTML elements and update them
     listItems += ` <div class="service__chosen">
       <div class="service__package">
           <h3 class="service__title">${services[i]}</h3>
@@ -60,23 +63,45 @@ function render(services) {
       <h3 class="service__cost"><span class="currency">$</span>${cost}</h3>
   </div>`;
   }
+  // Render totalAmount and serviceList with updated data
   servicesList.innerHTML = listItems;
-  totalAmount.innerHTML = "$" + totalCost;
+  totalAmount.innerHTML = `$${totalCost}`;
+  // Call update()
+  update(services);
+}
 
+// Create a function update() which updates style and note's checkout
+function update(services) {
   if (services.length > 0) {
     totalNotes.textContent = "We accept cash, credit card, or PayPal";
+  } else if (services.length === 0) {
+    totalNotes.textContent = "";
   }
   if (totalCost > 0) {
     totalAmount.style.color = "#4a4e74";
   }
+  if (totalCost === 0) {
+    totalAmount.style.color = "#918e9b";
+  }
 }
 
+// Create a function getCost() which returns service's cost
 function getCost(services) {
+  // The substring method will return the part of the string after the specified character ("$")
+  // However, we don't want to include the character in the returned string, so we increment its index by 1.
+  // Then use parseInt("", 10) to convert a string into an integer of the specified radix: in this case 10
   return parseInt(services.substring(services.indexOf("$") + 1), 10);
 }
 
-//function getTotalCost(services){
+// Listen for clicks on button
+sendBtn.addEventListener("click", function () {
+  reset();
+});
 
-//}
-
-console.log(myServices);
+// Create a function reset() which clears the whole cart
+function reset() {
+  // Clear totalCost, myServices and the DOM calling render()
+  totalCost = 0;
+  myServices = [];
+  render(myServices);
+}
